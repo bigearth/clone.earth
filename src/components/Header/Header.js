51e9c54069ae9a413b2e44c4f0d5d2f44366f3a4
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-05-03 19:35:02 -0700

    basic info.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,12 +22,11 @@ class Header extends React.Component {
         <div className={s.container}>
           <Navigation />
           <Link className={s.brand} to="/">
-            <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>&#9679; EARTH</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Clone</h1>
+            <p className={s.bannerDesc}>Create your world</p>
           </div>
         </div>
       </div>
