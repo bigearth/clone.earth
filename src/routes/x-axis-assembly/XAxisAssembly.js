7330b82a9040c66cbbabd471b1bd76a8ac6c0304
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-05-30 12:59:43 -0700

    Updates.

--- src/routes/x-axis-assembly/XAxisAssembly.js
+++ src/routes/x-axis-assembly/XAxisAssembly.js
@@ -14,7 +14,33 @@ class XAxisAssembly extends React.Component {
           <Col xs={12}>
             <div>
               <h2>X Axis Assembly</h2>
-              <p>Coming Soon</p>
+              <h3>Step 1 Gather materials</h3>
+              <h4>Tools</h4>
+              <ul>
+                <li></li>
+              </ul>
+              <h4>3D printed parts</h4>
+              <ul>
+                <li>X-carriage</li>
+                <li>X-end-motor</li>
+                <li>X-end-idler</li>
+              </ul>
+              <h4>Hardware</h4>
+              <ul>
+                <li>2.5mm Allen key</li>
+                <li>Needle-nose pliers</li>
+              </ul>
+              <img src='https://i.imgur.com/kNhiBiU.jpg' />
+              <hr />
+              <h3>Step 2 Assemble the Y-axis rods</h3>
+              <h4>Hardware</h4>
+              <ul>
+                <li></li>
+              </ul>
+              <ol>
+                <li></li>
+              </ol>
+              <img src='https://i.imgur.com/jdpvRQy.jpg' />
             </div>
           </Col>
         </Row>
