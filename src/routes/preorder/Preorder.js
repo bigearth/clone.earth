fcad11a2620e1c41d8557e8cbce7b8957a3b3046
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-06-16 18:52:33 -0700

    Preorder.

--- src/routes/preorder/Preorder.js
+++ src/routes/preorder/Preorder.js
@@ -3,19 +3,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Preorder.css';
-import { Jumbotron, Grid, Row, Col, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button, Image } from 'react-bootstrap';
 
 class Preorder extends React.Component {
   render() {
     return (
-      <Grid>
+      <Grid fluid className={s.root}>
         <Row>
           <Col xs={12}>
-            <div>
-              <h2>Coming Soon</h2>
-              <p>Thanks for being so enthusiastic! We appreciate it. Check back soon.</p>
-              <img src='https://s-media-cache-ak0.pinimg.com/originals/ce/3d/15/ce3d157d84562d1fcc45ac2e46cd363f.jpg' />
-            </div>
+            <h2>Coming Soon</h2>
+            <p>Thanks for being so enthusiastic! We appreciate it. Check back soon.</p>
+            <Image src="https://s-media-cache-ak0.pinimg.com/originals/ce/3d/15/ce3d157d84562d1fcc45ac2e46cd363f.jpg" />
           </Col>
         </Row>
       </Grid>
