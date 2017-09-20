5a51d8463e6ba9e2e5e5262578ff4fee71107569
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-20 10:19:11 -0700

    Landing page styles.

--- src/components/Html.js
+++ src/components/Html.js
@@ -36,6 +36,7 @@ class Html extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
           {styles.map(style =>
             <style
               key={style.id}
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,26 +4,66 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 import Link from '../../components/Link';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col, Image, Button, Glyphicon } from 'react-bootstrap';
 
 class Home extends React.Component {
   render() {
     return (
-      <Jumbotron className={s.root}>
+      <div>
+        <Jumbotron className={s.root}>
+          <Grid fluid>
+            <Row>
+              <Col xs={12} md={6} className={s.firstCol}>
+                <h1>Clone</h1>
+                <p>Make your world</p>
+                <Link to="/preorder"><Button bsSize="large"><Glyphicon glyph="bitcoin" /> Preorder</Button></Link>
+
+              </Col>
+              <Col xs={12} md={6}>
+                <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/home.jpg" circle />
+              </Col>
+             </Row>
+          </Grid>
+        </Jumbotron>
         <Grid fluid>
           <Row>
-            <Col xs={12} md={6} className={s.firstCol}>
-              <h1>Clone</h1>
-              <p>Make your world</p>
-              <Button bsSize="large"><Link to="/preorder">Preorder</Link></Button>
-
+            <Col xs={12} md={6}>
+              <h2>Quality Hardware</h2>
+              <p>Clone a desktop 3D printer for professionals. Built w/ the highest quality materials Clone will print all day every day.</p>
             </Col>
             <Col xs={12} md={6}>
-              <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/home.jpg" circle />
+              <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
+            </Col>
+           </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
+            </Col>
+            <Col xs={12} md={6}>
+              <h2>Quality Software</h2>
+              <p>Clone runs Marlin on a Rambo Mini. This ensures that the that the objects your want are the objects you get.</p>
+            </Col>
+           </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <h2>Print Anything</h2>
+              <p>Clone prints 3 types of plastic and wood or metal composites. It can print any design you need from tools to toys.</p>
+            </Col>
+            <Col xs={12} md={6}>
+              <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
+            </Col>
+           </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <Image src="oshw-logo.png" thumbnail />
+            </Col>
+            <Col xs={12} md={6}>
+              <h2>Open Source</h2>
+              <p>Clone is a member of the RepRap family. As such Clone&rsquo;s hardware and software are 100% open source enabling makers around the world.</p>
             </Col>
            </Row>
         </Grid>
-      </Jumbotron>
+      </div>
     );
   }
 }
