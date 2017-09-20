f32e9c9df5fc89ea0bff264e11e32d074bdbd042
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-20 08:36:46 -0700

    Responsive header.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,27 +4,30 @@ import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, Image } from 'react-bootstrap';
 
 class Navigation extends React.Component {
   render() {
     return (
-      <Navbar inverse className={s.root}>
+      <Navbar className={s.root} collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">EARTH</Link>
+            <Link to="/">
+              <Image src="EARTH-logo.png" />
+              EARTH</Link>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
-        <Nav>
-          <NavDropdown eventKey={3} title="Docs" id="docs-dropdown">
-            <MenuItem eventKey={3.1} href="/hardware">Hardware</MenuItem>
-            <MenuItem eventKey={3.1} href="/docs">Contribute</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={1} href="/about">About</NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={2} href="/preorder">Preorder</NavItem>
-        </Nav>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavDropdown eventKey={3} title="Docs" id="docs-dropdown">
+              <MenuItem eventKey={3.1} href="/hardware">Hardware</MenuItem>
+              <MenuItem eventKey={3.1} href="/docs">Contribute</MenuItem>
+            </NavDropdown>
+            <NavItem eventKey={1} href="/about">About</NavItem>
+            <NavItem eventKey={2} href="/preorder">Preorder</NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
