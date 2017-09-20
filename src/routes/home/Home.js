618555e24a7f699a23e8043437a971530802de92
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-20 10:54:33 -0700

    Homepage.

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -22,42 +22,42 @@ class Home extends React.Component {
               <Col xs={12} md={6}>
                 <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/home.jpg" circle />
               </Col>
-             </Row>
+            </Row>
           </Grid>
         </Jumbotron>
         <Grid fluid>
-          <Row>
-            <Col xs={12} md={6}>
+          <Row className={s.details}>
+            <Col xs={12} md={6} className={s.alignRight}>
               <h2>Quality Hardware</h2>
               <p>Clone a desktop 3D printer for professionals. Built w/ the highest quality materials Clone will print all day every day.</p>
             </Col>
-            <Col xs={12} md={6}>
+            <Col xs={12} md={6} className={s.alignLeft}>
               <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
             </Col>
            </Row>
-          <Row>
-            <Col xs={12} md={6}>
+          <Row className={s.details}>
+            <Col xs={12} md={6} className={s.alignRight}>
               <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
             </Col>
-            <Col xs={12} md={6}>
+            <Col xs={12} md={6} className={s.alignLeft}>
               <h2>Quality Software</h2>
               <p>Clone runs Marlin on a Rambo Mini. This ensures that the that the objects your want are the objects you get.</p>
             </Col>
            </Row>
-          <Row>
-            <Col xs={12} md={6}>
+          <Row className={s.details}>
+            <Col xs={12} md={6} className={s.alignRight}>
               <h2>Print Anything</h2>
               <p>Clone prints 3 types of plastic and wood or metal composites. It can print any design you need from tools to toys.</p>
             </Col>
-            <Col xs={12} md={6}>
+            <Col xs={12} md={6} className={s.alignLeft}>
               <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
             </Col>
            </Row>
-          <Row>
-            <Col xs={12} md={6}>
+          <Row className={s.details}>
+            <Col xs={12} md={6} className={s.alignRight}>
               <Image src="oshw-logo.png" thumbnail />
             </Col>
-            <Col xs={12} md={6}>
+            <Col xs={12} md={6} className={s.alignLeft}>
               <h2>Open Source</h2>
               <p>Clone is a member of the RepRap family. As such Clone&rsquo;s hardware and software are 100% open source enabling makers around the world.</p>
             </Col>
