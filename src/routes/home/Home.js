6482ab9c7e3fe6f89008a958cca715ee90fcfe4b
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-21 07:15:55 -0700

    layout.

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -24,23 +24,23 @@ class Home extends React.Component {
             </Row>
           </Grid>
         </Jumbotron>
-        <Grid fluid>
+        <Grid fluid className='hidden-xs'>
           <Row className={s.summary}>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} sm={6} lg={3}>
               <h3><Link to="#hardware"><Glyphicon glyph="wrench" /> Quality Hardware</Link></h3>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} sm={6} lg={3}>
               <h3><Link to="#software"><Glyphicon glyph="floppy-disk" /> Quality Software</Link></h3>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} sm={6} lg={3}>
               <h3><Link to="#anything"><Glyphicon glyph="star" /> Print Anything</Link></h3>
             </Col>
-            <Col xs={12} md={6} lg={3}>
+            <Col xs={12} sm={6} lg={3}>
               <h3><Link to="#opensource"><Glyphicon glyph="folder-open" /> Open Source</Link></h3>
             </Col>
           </Row>
         </Grid>
-        <Grid fluid>
+        <Grid fluid className='hidden-xs'>
           <Row className={s.pitch}>
             <Col xs={12}>
               <p>DESIGN AND CREATE NEW THINGS. UPGRADE AND FIX OLD THINGS.</p>
@@ -52,45 +52,45 @@ class Home extends React.Component {
         </Grid>
         <Grid fluid>
           <Row className={s.details} id='hardware'>
-            <Col xs={12} md={6} className={s.alignRight}>
+            <Col xs={12} sm={6} className={s.alignRight}>
               <h2>Quality Hardware</h2>
               <p>Clone is a desktop 3D printer for professionals. Built w/ the <Link to='/hardware'>highest quality materials</Link> Clone will print all day every day.</p>
             </Col>
-            <Col xs={12} md={6} className={s.alignLeft}>
+            <Col xs={12} sm={6} className={s.alignLeft}>
               <Link to='/hardware'>
                 <Image src="https://s3-us-west-1.amazonaws.com/www-clone-earth-assets/z-axis/step-1.jpg" thumbnail />
               </Link>
             </Col>
            </Row>
           <Row className={s.details} id='software'>
-            <Col xs={12} md={6} className={s.alignRight}>
+            <Col xs={12} sm={6} className={s.alignRight}>
               <a href='https://github.com/bigearth/Marlin'>
                 <Image src="marlin-logo.jpg" thumbnail />
               </a>
             </Col>
-            <Col xs={12} md={6} className={s.alignLeft}>
+            <Col xs={12} sm={6} className={s.alignLeft}>
               <h2>Quality Software</h2>
               <p>Clone runs <a href='https://github.com/bigearth/Marlin'>Marlin</a> on a Rambo Mini. This ensures that the objects you want are the objects you get.</p>
             </Col>
            </Row>
           <Row className={s.details} id='anything'>
-            <Col xs={12} md={6} className={s.alignRight}>
+            <Col xs={12} sm={6} className={s.alignRight}>
               <h2>Print Anything</h2>
               <p>Clone prints 3 types of plastic and wood or metal composites. It can print any design you need from <Link to='/examples'>tools to toys</Link>.</p>
             </Col>
-            <Col xs={12} md={6} className={s.alignLeft}>
+            <Col xs={12} sm={6} className={s.alignLeft}>
               <Link to='/examples'>
                 <Image src="board.jpg" thumbnail />
               </Link>
             </Col>
            </Row>
           <Row className={s.details} id='opensource'>
-            <Col xs={12} md={6} className={s.alignRight}>
+            <Col xs={12} sm={6} className={s.alignRight}>
               <Link to='/docs'>
                 <Image src="oshw-logo.png" thumbnail />
                </Link>
             </Col>
-            <Col xs={12} md={6} className={s.alignLeft}>
+            <Col xs={12} sm={6} className={s.alignLeft}>
               <h2>Open Source</h2>
               <p>Clone is a member of the RepRap family. As such Clone&rsquo;s hardware and software are <Link to='/docs'>100% open source</Link> enabling makers around the world.</p>
             </Col>
