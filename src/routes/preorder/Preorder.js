60a7e1bde0bed3af277c8825240033e15029954e
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-26 18:53:52 -0700

    Shapeshift integration.

--- src/routes/preorder/Preorder.js
+++ src/routes/preorder/Preorder.js
@@ -6,6 +6,28 @@ import s from './Preorder.css';
 import { Grid, Row, Col, Button, Image } from 'react-bootstrap';
 
 class Preorder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: 0,
+      shapeshift: ''
+    };
+    let that = this;
+    fetch('https://blockchain.info/ticker').then(function(response) {
+    	return response.json();
+    }).then(function(j) {
+      let last = j.USD.last;
+      fetch('https://shapeshift.io/rate/bch_btc').then(function(response) {
+      	return response.json();
+      }).then(function(j) {
+        let price = (900 / (last * j.rate)).toFixed(8);
+        that.setState({
+          price: price,
+          shapeshift: `https://shapeshift.io/shifty.html?destination=13RrATERfYvKfMTCGA6bfv5cBSzttts6gj&output=BCH&amount=${price}`});
+      });
+    });
+  }
+
   render() {
     return (
       <div>
@@ -21,7 +43,7 @@ class Preorder extends React.Component {
             <Col xs={12} sm={6} className={s.alignRight}>
               <p>We&rsquo;re launching Clone and are making preorders available. This is for people who believe in EARTH&rsquo;s vision and want to help us start this mission of a Clone for everyone on EARTH.</p>
               <p>We&rsquo;re initially only accepting Bitcoin Cash* because we believe in Satoshi&rsquo;s vision of a currency for the world&rsquo;s unbanked.</p>
-              <p>To order a clone send $900 USD in Bitcoin Cash to <strong>13RrATERfYvKfMTCGA6bfv5cBSzttts6gj</strong>. Then send the transaction id to <strong>buy@clone.earth</strong>. You&rsquo;ll get back a confirmation email shortly and your Clone will begin getting made.</p>
+              <p>To order a clone send <strong><span>{this.state.price}</span></strong> Bitcoin Cash to <strong>13RrATERfYvKfMTCGA6bfv5cBSzttts6gj</strong>. Then send the transaction id to <strong>buy@clone.earth</strong>. You&rsquo;ll get back a confirmation email shortly and your Clone will begin getting made.</p>
               <p>There is currently a 3 month lead time before the machines ship. Thank you for sharing our vision and taking the first step w/ us in making it a reality.</p>
             </Col>
             <Col xs={12} sm={6} className={s.alignLeft}>
@@ -36,6 +58,9 @@ class Preorder extends React.Component {
               <p>
                 <Image src="purchase-qr.jpg" thumbnail />
               </p>
+              <a href={this.state.shapeshift}>
+                <img src="https://shapeshift.io/images/shifty/small_light_altcoins.png" className="ss-button" />
+              </a>
             </Col>
           </Row>
         </Grid>
